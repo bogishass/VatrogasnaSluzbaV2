d2401bc3c3633df0db12dfdd79ea8f3427be0bcb
Bogdan Stojanovic <bogdanstojanovic2003@gmail.com>
2025-10-05 19:15:03 +0200

    DTOs 1

--- DTOs.cs
+++ DTOs.cs
@@ -16,9 +16,7 @@ namespace VatrogasnaSluzba
         public string Status { get; set; }
         public int? Kapacitet { get; set; }
         public DateTime? DatIstekaReg { get; set; }
-        public VoziloBasic(string regBroj, string tip, string podtip, 
-            string proizvodjac, int godProizvodnje, string status,
-            int kapacitet, DateTime datIstekaReg) 
+        public VoziloBasic(string regBroj, string tip, string podtip, string proizvodjac, int godProizvodnje, string status, int kapacitet, DateTime datIstekaReg) 
         {
             RegBroj = regBroj;
             Tip = tip;
@@ -30,4 +28,45 @@ namespace VatrogasnaSluzba
             DatIstekaReg = datIstekaReg;
         }
     }
+
+    public class VoziloVolonteraBasic
+    {
+        public string RegBroj { get; set; }
+        public string Tip { get; set; }
+        public string Marka { get; set; }
+
+        public VoziloVolonteraBasic(string regBroj, string tip, string marka)
+        {
+            RegBroj = regBroj;
+            Tip = tip;
+            Marka = marka;
+        }
+    }
+
+    public class VolonterBasic
+    {
+        public string MaticniBroj { get; set; }
+        public string Ime { get; set; }
+        public  string Prezime { get; set; }
+        public  string Pol { get; set; }
+        public  DateTime DatumRodjenja { get; set; }
+        public  string Adresa { get; set; }
+        public  string Email { get; set; }
+        public  IList<string> Telefoni { get; set; } = new List<string>();
+
+        public VolonterBasic(string maticniBroj, string ime, string prezime, string pol, DateTime datumRodjenja, string adresa, string email, IList<string> telefoni)
+        {
+            MaticniBroj = maticniBroj;
+            Ime = ime;
+            Prezime = prezime;
+            Pol = pol;
+            DatumRodjenja = datumRodjenja;
+            Adresa = adresa;
+            Email = email;
+            Telefoni = telefoni;
+        }
+        
+    }
+
+
 }
